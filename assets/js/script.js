9985dd5373a21ab4a6f4e1239644d971b064f49b
Rockojoe2 <lee.d.joey@gmail.com>
2023-07-03 17:18:53 -0500

    Made the pseudoscript for code

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+/*Pseudoscript
+
+Start off with a page that introduces them to the quiz with a buttom that activates on click to continue to quiz
+
+Have a timer at the top right that starts at 80 seconds.
+
+Have the first question appear, and then have the timer goes down
+
+If user answers correctly, move on to the next question
+
+If user doesn't answer correctly, we subtract some time from the clock.
+
+The game is over when the user answers all the questions, or the timer hits 0, and whatever time is left will be the users score.
+
+Give the user their score and ask user to put in their initials. 
+
+
+*/
