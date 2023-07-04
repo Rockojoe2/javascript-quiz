18a44d599b829870911b6af682c839484721d835
Rockojoe2 <lee.d.joey@gmail.com>
2023-07-03 19:18:50 -0500

    Started over JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,9 +24,9 @@ if statements for the question and answer, if question is correct the nmove on, 
 
 const questionBank = [
     {
-        question: "This is the first question",
+        question: "This is the first question. How many Pokemon are there in the First Generation",
         answers: [
-            { text: "Answer Choice 1", correct: true},
+            { text: "151", correct: true},
             { text: "Answer Choice 2", correct: false},
             { text: "Answer Choice 3", correct: false},
             { text: "Answer Choice 4", correct: false},
@@ -44,51 +44,94 @@ const questionBank = [
     }
 ];
 
-console.log(questionBank[0]); //Checking to see if my question bank is working
+//console.log(questionBank[0]); //Checking to see if my question bank is working
 
 var question = document.getElementById("question");
 var answerButton = document.getElementById("answer-button");
 var nextButton = document.getElementById("next-button");
+var selectedAnswer1 = document.getElementById("first-choice");
+var selectedAnswer2 = document.getElementById("second-choice");
+var selectedAnswer3 = document.getElementById("third-choice");
+var selectedAnswer4 = document.getElementById("fourth-choice");
 
 var currentQuestionNumber = 0;
 let score = 0;
 
+function displayQuestion(){
+
+    question.textContent = questionBank[currentQuestionNumber].question;
+    selectedAnswer1.textContent = questionBank[currentQuestionNumber].answers[0].text;
+    selectedAnswer2.textContent = questionBank[currentQuestionNumber].answers[1].text;
+    selectedAnswer3.textContent = questionBank[currentQuestionNumber].answers[2].text;
+    selectedAnswer4.textContent = questionBank[currentQuestionNumber].answers[3].text;
+
+}
+
+
+
+displayQuestion();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//OLD CODE BELOW
+
+
 //console.log("Var question is: " + question); //H2 
 
-function startQuiz(){
-    currentQuestionNumber = 0;
-    score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestion();
-}
+// function startQuiz(){
+//     currentQuestionNumber = 0;
+//     score = 0;
+//     nextButton.innerHTML = "Next";
+//     showQuestion();
+// }
 
-function showQuestion(){
-    resetButtons(); //Resets the current state and gets rid of all buttons that are existing
-    var currentQuestion = questionBank[currentQuestionNumber];
-    //console.log("Current question is: " + currentQuestion.question);
-    var questionNumber = currentQuestionNumber + 1;
-    question.innerHTML = questionNumber + "." + currentQuestion.question;
+// function showQuestion(){
+//     resetButtons(); //Resets the current state and gets rid of all buttons that are existing
+//     var currentQuestion = questionBank[currentQuestionNumber];
+//     //console.log("Current question is: " + currentQuestion.question);
+
+//     var questionNumber = currentQuestionNumber + 1;
+//     question.innerHTML = questionNumber + "." + currentQuestion.question;
+
+
    
 
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButton.appendChild(button);
-    })
-        
-    };
+//     currentQuestion.answers.forEach(answer => {
+//         //console.log(answer);
+//         const button = document.createElement("button");
+//         button.innerHTML = answer.text;
+//         button.classList.add("btn");
+//         answerButton.appendChild(button);
+//     })
+//     };
 
  
-//Will reset the button state every time we go to the next question.
-function resetButtons(){
-    nextButton.style.display = "none";
-    while(answerButton.firstChild){
-        answerButton.removeChild(answerButton.firstChild);
-    }
-}
+// //Will reset the button state every time we go to the next question.
+// function resetButtons(){
+//     nextButton.style.display = "none";
+//     while(answerButton.firstChild){
+//         answerButton.removeChild(answerButton.firstChild);
+//     }
+// }
 
-startQuiz();
+
+
+// startQuiz();
+
+
 
 
 
